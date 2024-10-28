06a57a7d3ec03a6ff6dd1763c2039b3e4057e7be
nukko <shouta.tsuboi@gmail.com>
2024-10-28 14:50:09 +0900

    データの新規作成を追加

--- app/features/resources/components/ResourceCreateButton.tsx
+++ app/features/resources/components/ResourceCreateButton.tsx
@@ -1,51 +1,29 @@
 'use client'
 
 import { useState } from 'react'
-import { Button } from 'react-aria-components'
+import { Button, Dialog, DialogTrigger, Popover } from 'react-aria-components'
 import { FilePlus } from 'lucide-react'
+import ResourceCreateForm from '@/app/features/resources/components/ResourceCreateForm'
 
 const ResourceCreateButton = () => {
-	const [isLoading, setIsLoading] = useState(false)
-	const [error, setError] = useState<string | null>(null)
-
-	const onClick = async () => {
-		try {
-			setIsLoading(true)
-
-			const response = await fetch('/api/resources', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					title: 'Untitled',
-					url: 'https://example.com',
-					position: 3,
-				}),
-			})
-
-			setIsLoading(false)
-			if (!response.ok) {
-				const errorData = await response.json()
-				throw new Error(errorData.error || 'Failed to create resource')
-			}
-			const resource = await response.json()
-			console.log('Resource created:', resource)
-		} catch (error) {
-			console.error('Error creating resource:', error)
-			setError(
-				error instanceof Error ? error.message : 'Failed to create resource',
-			)
-		} finally {
-			setIsLoading(false)
-		}
-	}
+	const [isOpen, setIsOpen] = useState<boolean>(false)
 
 	return (
 		<div className="mr-4">
-			<Button type="button" onPress={onClick}>
-				<FilePlus className="w-6 h-6 text-zinc-700" />
-			</Button>
+			<DialogTrigger isOpen={isOpen} onOpenChange={setIsOpen}>
+				<Button aria-label="Menu" className="outline-none p-2">
+					<FilePlus className="w-6 h-6 text-zinc-700" />
+				</Button>
+				<Popover>
+					<Dialog className="outline-none">
+						<div>
+							<div className="bg-white flex items-center justify-center rounded-lg shadow-md">
+								<ResourceCreateForm onClose={() => setIsOpen(false)} />
+							</div>
+						</div>
+					</Dialog>
+				</Popover>
+			</DialogTrigger>
 		</div>
 	)
 }
