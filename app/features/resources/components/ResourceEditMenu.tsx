a6bae966f2bb4d767f847f961475f8cdda6d1d8c
nukko <shouta.tsuboi@gmail.com>
2024-10-28 14:53:15 +0900

    Modalに変更

--- app/features/resources/components/ResourceEditMenu.tsx
+++ app/features/resources/components/ResourceEditMenu.tsx
@@ -3,35 +3,32 @@
 import {
 	Button,
 	DialogTrigger,
-	Popover,
 	Dialog,
 	Modal,
+	ModalOverlay,
 } from 'react-aria-components'
 import ResourceEditForm from '@/app/features/resources/components/ResourceEditForm'
 import type { Resource } from '@prisma/client'
-import { useState } from 'react'
 import { Pencil } from 'lucide-react'
 const ResourceEditMenu = ({
 	resource,
 }: { resource: Pick<Resource, 'id' | 'title' | 'description' | 'url'> }) => {
-	const [isOpen, setIsOpen] = useState(false)
 	return (
-		<DialogTrigger isOpen={isOpen} onOpenChange={setIsOpen}>
-			<Button aria-label="Menu" className="outline-none pl-4 pr-2 py-4">
+		<DialogTrigger>
+			<Button aria-label="Edit" className="outline-none pl-4 pr-2 py-4">
 				<Pencil className="w-5 h-5" />
 			</Button>
-			<Popover className="">
-				<Dialog className="outline-none">
-					<div>
-						<div className="bg-white flex items-center justify-center rounded-lg shadow-md">
-							<ResourceEditForm
-								resource={resource}
-								onClose={() => setIsOpen(false)}
-							/>
-						</div>
-					</div>
-				</Dialog>
-			</Popover>
+			<ModalOverlay className="fixed inset-0 z-50 bg-black/20 items-center justify-center">
+				<Modal className="fixed inset-0 flex items-center justify-center outline-none">
+					<Dialog className="outline-none">
+						{({ close }) => (
+							<div className="bg-white flex items-center justify-center rounded-lg shadow-md">
+								<ResourceEditForm resource={resource} onClose={close} />
+							</div>
+						)}
+					</Dialog>
+				</Modal>
+			</ModalOverlay>
 		</DialogTrigger>
 	)
 }
