7c90c3ef71f26a9fcee3dc7a4b7fbd8827f9ec17
nukko <shouta.tsuboi@gmail.com>
2024-10-28 14:10:59 +0900

    アイコンの追加

--- app/features/resources/components/ResourceEditMenu.tsx
+++ app/features/resources/components/ResourceEditMenu.tsx
@@ -1,19 +1,26 @@
 'use client'
 
-import { Button, DialogTrigger, Popover, Dialog } from 'react-aria-components'
+import {
+	Button,
+	DialogTrigger,
+	Popover,
+	Dialog,
+	Modal,
+} from 'react-aria-components'
 import ResourceEditForm from '@/app/features/resources/components/ResourceEditForm'
 import type { Resource } from '@prisma/client'
 import { useState } from 'react'
+import { Pencil } from 'lucide-react'
 const ResourceEditMenu = ({
 	resource,
 }: { resource: Pick<Resource, 'id' | 'title' | 'description' | 'url'> }) => {
 	const [isOpen, setIsOpen] = useState(false)
 	return (
 		<DialogTrigger isOpen={isOpen} onOpenChange={setIsOpen}>
-			<Button aria-label="Menu" className="outline-none p-2">
-				☰
+			<Button aria-label="Menu" className="outline-none pl-4 pr-2 py-4">
+				<Pencil className="w-5 h-5" />
 			</Button>
-			<Popover>
+			<Popover className="">
 				<Dialog className="outline-none">
 					<div>
 						<div className="bg-white flex items-center justify-center rounded-lg shadow-md">
