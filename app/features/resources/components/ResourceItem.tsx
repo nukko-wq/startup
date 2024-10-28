f9305110c1d1c62a9c8b60436af70b1689adedf5
nukko <shouta.tsuboi@gmail.com>
2024-10-28 09:25:13 +0900

    React AriaのLinkコンポーネントを使用

--- app/features/resources/components/ResourceItem.tsx
+++ app/features/resources/components/ResourceItem.tsx
@@ -1,6 +1,8 @@
+'use client'
+
 import type { Resource } from '@prisma/client'
 import ResourceEditMenu from '@/app/features/resources/components/ResourceEditMenu'
-import Link from 'next/link'
+import { Link } from 'react-aria-components'
 
 interface ResourceItemProps {
 	resource: Pick<Resource, 'id' | 'title' | 'description' | 'url'>
@@ -10,7 +12,7 @@ export default function ResourceItem({ resource }: ResourceItemProps) {
 	return (
 		<div className="flex justify-between items-center p-1 border-b border-gray-200 last:border-b-0 hover:bg-zinc-100">
 			<div className="flex flex-grow flex-col p-1 ml-1">
-				<Link href={resource.url} target="_blank" className="">
+				<Link href={resource.url} target="_blank" className="outline-none">
 					<div>{resource.title}</div>
 					<div className="text-sm text-muted-foreground">
 						{resource.description}
