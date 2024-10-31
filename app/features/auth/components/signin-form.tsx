e6d39630967ad4ce3a26e201d5dffeb57240be12
nukko <shouta.tsuboi@gmail.com>
2024-10-31 20:06:41 +0900

    クライアントとサーバーのコンポーネントを分離

--- app/features/auth/components/signin-form.tsx
+++ app/features/auth/components/signin-form.tsx
@@ -1,21 +1,20 @@
-import { auth, signIn } from '@/lib/auth'
+'use client'
+
+import { signIn } from '@/lib/auth'
 import SignInButton from '@/app/features/auth/components/signin-button'
 
-export default async function SignInForm() {
-	const session = await auth()
-
+export default function SignInForm() {
 	return (
 		<div className="w-[350px] border border-border rounded-lg">
 			<form
 				className="flex justify-center"
-				action={async () => {
-					'use server'
+				onSubmit={async (e) => {
+					e.preventDefault()
 					await signIn('google')
 				}}
 			>
 				<SignInButton />
 			</form>
-			{session?.user?.name}
 		</div>
 	)
 }
