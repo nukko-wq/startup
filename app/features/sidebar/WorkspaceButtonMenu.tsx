09d85afefaa4b19bc79b9173cc93a976cceb54b6
nukko <shouta.tsuboi@gmail.com>
2024-11-10 08:35:30 +0900

    Styleの調整

--- app/features/sidebar/WorkspaceButtonMenu.tsx
+++ app/features/sidebar/WorkspaceButtonMenu.tsx
@@ -31,26 +31,26 @@ const WorkspaceButtonMenu = ({
 			<MenuTrigger>
 				<Button
 					aria-label="Menu"
-					className="outline-none p-2 mr-2 group-hover:bg-gray-600 transition duration-200 rounded-full opacity-0 group-hover:opacity-100"
+					className="outline-none p-1 mr-2 group-hover:bg-gray-700 transition duration-200 rounded-full opacity-0 group-hover:opacity-100"
 				>
-					<EllipsisVertical className="w-4 h-4 text-zinc-50" />
+					<EllipsisVertical className="w-5 h-5 text-zinc-50" />
 				</Button>
 				<Popover>
-					<Menu className="bg-zinc-50 outline-none border rounded-lg shadow-md">
+					<Menu className="bg-zinc-50 outline-none border rounded-sm shadow-md min-w-[160px]">
 						<MenuItem
 							onAction={() => setIsRenameOpen(true)}
-							className="pl-3 pr-4 py-2 outline-none hover:bg-zinc-100 hover:cursor-pointer"
+							className="pl-4 pr-5 py-2 outline-none hover:bg-zinc-100 hover:cursor-pointer text-sm"
 						>
-							<div className="flex items-center gap-2">
+							<div className="flex items-center gap-3">
 								<Pencil className="w-4 h-4" />
 								Rename
 							</div>
 						</MenuItem>
 						<MenuItem
 							onAction={() => setIsDeleteOpen(true)}
-							className="pl-3 pr-4 py-2 outline-none hover:bg-zinc-100 text-red-600 hover:cursor-pointer"
+							className="pl-4 pr-5 py-2 outline-none hover:bg-zinc-100 text-red-600 hover:cursor-pointer text-sm"
 						>
-							<div className="flex items-center gap-2">
+							<div className="flex items-center gap-3">
 								<Trash2 className="w-4 h-4" />
 								Delete
 							</div>
