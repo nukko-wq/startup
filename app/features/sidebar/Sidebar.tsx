54188da0b7001d1d7575994a147b76649100a1e2
nukko <shouta.tsuboi@gmail.com>
2024-11-10 09:53:38 +0900

    Styleの修正

--- app/features/sidebar/Sidebar.tsx
+++ app/features/sidebar/Sidebar.tsx
@@ -41,7 +41,7 @@ export default function Sidebar({
 	])
 
 	return (
-		<div className="w-[320px] bg-gray-800 h-screen flex flex-col">
+		<div className="hidden md:flex w-[320px] bg-gray-800 h-screen flex-col">
 			<div className="flex items-center justify-between pl-4 pr-3 pt-4 pb-4">
 				<Link
 					href="/"
