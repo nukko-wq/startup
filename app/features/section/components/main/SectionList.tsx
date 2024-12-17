37a6f600c085fbd49e9222e1c750395edf55cb95
nukko <shouta.tsuboi@gmail.com>
2024-12-17 14:52:03 +0900

    SectionItem.tsxをSectionList.tsxに統合

--- app/features/section/components/main/SectionList.tsx
+++ app/features/section/components/main/SectionList.tsx
@@ -1,8 +1,48 @@
 'use client'
 
 import { useAppSelector } from '@/app/lib/redux/hooks'
-import SectionItem from '@/app/features/section/components/main/SectionItem'
+import { File } from 'lucide-react'
+import { useRef } from 'react'
 import { selectSectionsByActiveSpace } from '@/app/lib/redux/features/section/selector'
+import type { Section } from '@/app/lib/redux/features/section/types/section'
+import SectionMenu from '@/app/features/section/components/main/SectionMenu'
+import SectionNameEdit from '@/app/features/section/components/main/SectionNameEdit'
+import ResourceList from '@/app/features/resource/components/main/ResourceList'
+import ResourceCreateButton from '@/app/features/resource/components/main/ResourceCreateButton'
+
+interface SectionItemProps {
+	section: Section
+}
+
+const SectionItem = ({ section }: SectionItemProps) => {
+	const resourceCreateButtonRef = useRef<HTMLButtonElement>(null)
+
+	const handleAddResourceClick = () => {
+		resourceCreateButtonRef.current?.click()
+	}
+
+	return (
+		<div className="min-w-[260px] max-w-[920px] w-full pl-[16px] pr-[32px] py-5 outline-none">
+			<div className="flex justify-between items-center mb-2">
+				<div className="flex items-center ml-4 cursor-grab" slot="drag">
+					<File className="w-6 h-6 text-slate-700" />
+					<SectionNameEdit section={section} />
+				</div>
+				<div className="hidden md:flex">
+					<ResourceCreateButton
+						ref={resourceCreateButtonRef}
+						section={section}
+					/>
+					<SectionMenu
+						section={section}
+						onAddResourceClick={handleAddResourceClick}
+					/>
+				</div>
+			</div>
+			<ResourceList sectionId={section.id} />
+		</div>
+	)
+}
 
 const SectionList = () => {
 	const sections = useAppSelector(selectSectionsByActiveSpace)
