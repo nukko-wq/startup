d033269997e3f63f34adad2ed2d6a0e281ef6b52
nukko <shouta.tsuboi@gmail.com>
2024-12-19 21:06:51 +0900

    ファイルを分離

--- app/(dashboard)/utils/fetchInitialData.ts
+++ app/(dashboard)/utils/fetchInitialData.ts
@@ -78,3 +78,59 @@ export async function fetchInitialData(userId: string): Promise<InitialData> {
 		activeSpace,
 	}
 }
+
+export async function fetchSpaceData(
+	userId: string,
+	spaceId: string,
+): Promise<{
+	initialWorkspace: WorkspaceWithSpacesAndSections[]
+}> {
+	// スペースの存在確認
+	const spaceExists = await prisma.space.findUnique({
+		where: { id: spaceId },
+	})
+
+	if (!spaceExists) {
+		throw new Error('Space not found')
+	}
+
+	// 現在のスペースをアクティブに設定
+	await prisma.space.update({
+		where: { id: spaceId },
+		data: { isLastActive: true },
+	})
+
+	// 他のスペースのisLastActiveをfalseに設定
+	await prisma.space.updateMany({
+		where: {
+			AND: [{ id: { not: spaceId } }, { userId }],
+		},
+		data: { isLastActive: false },
+	})
+
+	// ワークスペースとスペースのデータを取得
+	const initialWorkspace = await prisma.workspace.findMany({
+		where: { userId },
+		orderBy: { order: 'asc' },
+		include: {
+			spaces: {
+				orderBy: { order: 'asc' },
+				include: {
+					sections: {
+						orderBy: { order: 'asc' },
+						where: {
+							spaceId: spaceId,
+						},
+						include: {
+							resources: {
+								orderBy: { order: 'asc' },
+							},
+						},
+					},
+				},
+			},
+		},
+	})
+
+	return { initialWorkspace }
+}
