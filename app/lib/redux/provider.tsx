ba3af7cfec335d56643d3360b318f97291482796
nukko <shouta.tsuboi@gmail.com>
2025-05-12 14:36:51 +0900

    reduxのアップデート

--- app/lib/redux/provider.tsx
+++ app/lib/redux/provider.tsx
@@ -1,7 +1,7 @@
 'use client'
 
+import { store } from '@/app/lib/redux/store'
 import { Provider } from 'react-redux'
-import { store } from './store'
 
 export function ReduxProvider({ children }: { children: React.ReactNode }) {
 	return <Provider store={store}>{children}</Provider>
